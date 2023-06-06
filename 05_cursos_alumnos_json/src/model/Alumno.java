1676271f064cc557f191c3927f36c4f323eedaae
Antonio Martín <ajms66@gmail.com>
2023-06-06 14:48:45 +0200

    java beans completos

--- 05_cursos_alumnos_json/src/model/Alumno.java
+++ 05_cursos_alumnos_json/src/model/Alumno.java
@@ -1,18 +1,15 @@
 package model;
 
-import java.util.List;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Setter
 @Getter
-public class Curso {
-	private String curso;
-	private int duracion;
-	private double precio;
-	private List<Alumno> alumnos;
+public class Alumno {
+	private String nombre;
+	private int edad;
 }
---
+++ 05_cursos_alumnos_json/src/service/CursosService.java
@@ -0,0 +1,15 @@
+package service;
+
+public class CursosService {
+
+	//cursos precio máximo: Recibe un precio máximo
+	//y devuelve la lista de cursos cuyo precio sea inferior o igual a ese valor
+	//ordenados de menor a mayor precio
+	
+	
+	//a partir del nombre de un curso, devuelve la lista de alumnos de dicho curso
+	
+	//duración media de todos los cursos
+	
+	//media de edad de todos los alumnos
+}
