1a75d1387cb20176526d3c9d26cf8f5ccda9114e
Antonio Martín <ajms66@gmail.com>
2023-06-09 14:55:33 +0200

    ejercicios 1_19

--- 18_pedidos_nueva_multitarea/src/principal/Lanzador.java
+++ 18_pedidos_nueva_multitarea/src/principal/Lanzador.java
@@ -1,5 +1,8 @@
 package principal;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import tareas.TareaTienda;
 
 public class Lanzador {
@@ -8,10 +11,12 @@ public class Lanzador {
 		TareaTienda t1=new TareaTienda("c:\\tempo\\tienda1.txt","tienda1");
 		TareaTienda t2=new TareaTienda("c:\\tempo\\tienda2.txt","tienda2");
 		TareaTienda t3=new TareaTienda("c:\\tempo\\tienda3.txt","tienda3");
-		new Thread(t1).start();
-		new Thread(t2).start();
-		new Thread(t3).start();		
+		ExecutorService executor=Executors.newCachedThreadPool();
+		executor.submit(t1);
+		executor.submit(t2);
+		executor.submit(t3);
 		System.out.println("tareas en proceso!");
+		executor.shutdown();
 	}
 
 }
