94febbd9e8acf93cea42c4b2c5472e4f6ee69665
Antonio Martín <ajms66@gmail.com>
2023-06-09 14:23:51 +0200

    commit hasta ejercicio 17

---
+++ 16_contador_multitarea/src/principal/Lanzador.java
@@ -0,0 +1,17 @@
+package principal;
+
+import service.Contador;
+import tareas.TareaVisita;
+
+public class Lanzador {
+
+	public static void main(String[] args) throws InterruptedException {
+		Contador contador=new Contador();
+		for(int i=1;i<=5;i++) {
+			new Thread(new TareaVisita(contador)).start();
+		}
+		Thread.sleep(10000);
+		System.out.println("Visitar actuales: "+contador.getValor());
+	}
+
+}
